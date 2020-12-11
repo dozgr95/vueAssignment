f79f28b6ac295ce3cb36181132297af1b48e836e
dozgr95 <dominik.zgraggen@hotmail.com>
2020-12-11 09:12:02 +0100

    implement percentage view

---
+++ todo-app/src/views/Percentage.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="percentage">
+    <h3>Done:</h3>
+    <v-progress-linear
+      value="getDonePercentage()"
+      height="25"
+    >
+      <strong>{{ Math.ceil(getDonePercentage()) }}%</strong>
+    </v-progress-linear>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {
+
+  },
+})
+export default class Percentage extends Vue {
+  public getDonePercentage() {
+    return this.$store.getters.donePercentage;
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .percentage {
+    margin: 20px 0;
+    display:flex;
+  }
+  .v-progress-linear {
+    margin-left: 10px;
+    border: black 1px solid;
+  }
+</style>
