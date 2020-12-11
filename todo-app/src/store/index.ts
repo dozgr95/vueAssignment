29604afc264ef3b56a83b9dea54fd079484bc773
dozgr95 <dominik.zgraggen@hotmail.com>
2020-12-11 10:11:45 +0100

    add local storage

--- todo-app/src/store/index.ts
+++ todo-app/src/store/index.ts
@@ -29,12 +29,7 @@ export const getters = {
 };
 
 export const mutations = {
-  createToDo(state: AppState, title: string) {
-    const todo: Todo = {
-      id: Guid.create().toString(),
-      title,
-      done: false,
-    };
+  createToDo(state: AppState, todo: Todo) {
     state.todos.push(todo);
   },
   deleteToDo(state: AppState, id: string) {
@@ -45,14 +40,33 @@ export const mutations = {
     const itemId = state.todos.findIndex((todo) => todo.id === id);
     state.todos[itemId].done = true;
   },
+  saveLoadedTodos(state: AppState, todos: Todo[]) {
+    state.todos = todos;
+  },
+};
+
+export const actions = {
+  loadStorage({ commit }: {commit: Function}) {
+    const storage: string|null = localStorage.getItem('vue-app-todos');
+    const todos: Todo[] = storage ? JSON.parse(storage) : [];
+    commit('saveLoadedTodos', todos);
+  },
+  addTodo({ state, commit }: {state: AppState; commit: Function}, title: string) {
+    const todo: Todo = {
+      id: Guid.create().toString(),
+      title,
+      done: false,
+    };
+    commit('createToDo', todo);
+    localStorage.setItem('vue-app-todos', JSON.stringify(state.todos));
+  },
 };
 
 export default new Vuex.Store({
   state: baseState,
   mutations,
   getters,
-  actions: {
-  },
+  actions,
   modules: {
   },
 });
