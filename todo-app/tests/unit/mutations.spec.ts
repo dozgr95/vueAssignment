dfff94e159cc3d74b558ac8e569e26f0c06f642c
dozgr95 <dominik.zgraggen@hotmail.com>
2020-12-11 11:07:18 +0100

    add test for set done

---
+++ todo-app/tests/unit/mutations.spec.ts
@@ -0,0 +1,30 @@
+import { mutations, Todo } from '../../src/store';
+
+const { setDone } = mutations;
+
+describe('mutation setDone', () => {
+  it('should set to true', () => {
+    const state = {
+      todos: [
+        {
+          id: 'first',
+          done: false
+        } as Todo
+      ],
+    };
+    setDone(state, 'first');
+    expect(state.todos[0].done).toEqual(true);
+  });
+  it('should set to false', () => {
+    const state = {
+      todos: [
+        {
+          id: 'first',
+          done: true
+        } as Todo
+      ],
+    };
+    setDone(state, 'first');
+    expect(state.todos[0].done).toEqual(false);
+  });
+});
