84fd2d5b798629fe17a421c87845bbe6bc71bb11
dozgr95 <dominik.zgraggen@hotmail.com>
2020-12-11 09:06:13 +0100

    implement store with getter test

--- todo-app/src/store/index.ts
+++ todo-app/src/store/index.ts
@@ -1,13 +1,56 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { Guid } from 'guid-typescript';
 
 Vue.use(Vuex);
 
+export interface Todo {
+  id: string;
+  title: string;
+  done: boolean;
+}
+
+export interface AppState {
+  todos: Todo[];
+}
+
+export const baseState = {
+  todos: [],
+} as AppState;
+
+export const getters = {
+  // return percentage (int without %) of todos that had been marked as done
+  donePercentage(state: AppState) {
+    if (state.todos.length > 0) {
+      return (state.todos.filter((todo) => todo.done).length / state.todos.length) * 100;
+    }
+    return 0;
+  },
+};
+
+export const mutations = {
+  createToDo(state: AppState, title: string) {
+    const todo: Todo = {
+      id: Guid.create().toString(),
+      title,
+      done: false,
+    };
+    state.todos.push(todo);
+  },
+  deleteToDo(state: AppState, id: string) {
+    const itemId = state.todos.findIndex((todo) => todo.id === id);
+    state.todos.splice(itemId, 1);
+  },
+  markAsDone(state: AppState, id: string) {
+    const itemId = state.todos.findIndex((todo) => todo.id === id);
+    state.todos[itemId].done = true;
+  },
+};
+
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
+  state: baseState,
+  mutations,
+  getters,
   actions: {
   },
   modules: {
---
+++ todo-app/tests/unit/getters.spec.ts
@@ -0,0 +1,39 @@
+import { getters, Todo } from '../../src/store'
+
+const { donePercentage } = getters
+
+describe('getter donePercentage', () => {
+  it('should return 0 and not nAn', () => {
+    const state = {
+        todos: []
+    }
+    const result = donePercentage(state)
+    expect(result).toEqual(0);
+  });
+  it('should return 0 when none are done', () => {
+    const state = {
+        todos: [
+            {
+                id: 'someId',
+                title: 'Here is the title of done Todo',
+                done: false
+            } as Todo
+        ]
+    }
+    const result = donePercentage(state)
+    expect(result).toEqual(0);
+  });
+  it('should return 100 all are done', () => {
+    const state = {
+        todos: [
+            {
+                id: 'someId',
+                title: 'Here is the title of done Todo',
+                done: true
+            } as Todo
+        ]
+    }
+    const result = donePercentage(state)
+    expect(result).toEqual(100);
+  });
+});
