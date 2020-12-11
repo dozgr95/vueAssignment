41bbd4500c328cb0261cc9b209edce4a35a60241
dozgr95 <dominik.zgraggen@hotmail.com>
2020-12-11 10:11:53 +0100

    lint tests

--- todo-app/tests/unit/getters.spec.ts
+++ todo-app/tests/unit/getters.spec.ts
@@ -1,39 +1,39 @@
-import { getters, Todo } from '../../src/store'
+import { getters, Todo } from '../../src/store';
 
-const { donePercentage } = getters
+const { donePercentage } = getters;
 
 describe('getter donePercentage', () => {
   it('should return 0 and not nAn', () => {
     const state = {
-        todos: []
-    }
-    const result = donePercentage(state)
+      todos: [],
+    };
+    const result = donePercentage(state);
     expect(result).toEqual(0);
   });
   it('should return 0 when none are done', () => {
     const state = {
-        todos: [
+      todos: [
             {
-                id: 'someId',
-                title: 'Here is the title of done Todo',
-                done: false
-            } as Todo
-        ]
-    }
-    const result = donePercentage(state)
+              id: 'someId',
+              title: 'Here is the title of done Todo',
+              done: false,
+            } as Todo,
+      ],
+    };
+    const result = donePercentage(state);
     expect(result).toEqual(0);
   });
   it('should return 100 all are done', () => {
     const state = {
-        todos: [
+      todos: [
             {
-                id: 'someId',
-                title: 'Here is the title of done Todo',
-                done: true
-            } as Todo
-        ]
-    }
-    const result = donePercentage(state)
+              id: 'someId',
+              title: 'Here is the title of done Todo',
+              done: true,
+            } as Todo,
+      ],
+    };
+    const result = donePercentage(state);
     expect(result).toEqual(100);
   });
 });
